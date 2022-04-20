41dc09ac5d11128623739c52ca6ceb9cb22b2fb2
Daniel Iversen <daiverse@stud.ntnu.no>
2022-04-20 20:54:31 +0200

    Calculates Mass, Arm and MassArm for Zlin

--- FlightPlanning_UTSA/Plane.cs
+++ FlightPlanning_UTSA/Plane.cs
@@ -2,9 +2,81 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace FlightPlanning_UTSA
 {
     class Plane
     {
+        //Mass
+        public double BEM_Mass {get; set;}
+        public double LeftSeat_Mass {get; set;}
+        public double RightSeat_Mass {get; set;}
+        public double FuelMain_Mass {get; set;}
+        public double FuelAux_Mass {get; set;}
+        public double Baggage_Mass {get; set;}
+
+        //Arm
+        public double BEM_Arm {get; set;}
+        public double LeftSeat_Arm {get; set;}
+        public double RightSeat_Arm {get; set;}
+        public double FuelMain_Arm {get; set;}
+        public double FuelAux_Arm {get; set;}
+        public double Baggage_Arm {get; set;}
+
+        //MassArm
+        public double BEM_MassArm {get; set;}
+        public double LeftSeat_MassArm {get; set;}
+        public double RightSeat_MassArm {get; set;}
+        public double FuelMain_MassArm {get; set;}
+        public double FuelAux_MassArm {get; set;}
+        public double Baggage_MassArm {get; set;}
+
+        public double total_Mass {get; set;} = 0;
+        public double total_MassArm{get; set;} = 0;
+        public double total_Arm{get; set;} = 0;
+
+
+        public Plane() {
+            
+        }
+
+        public Plane(double bEM_Arm, double leftSeat_Arm, double rightSeat_Arm, double fuelMain_Arm, double fuelAux_Arm, double baggage_Arm)
+            {
+                BEM_Arm = bEM_Arm;
+                LeftSeat_Arm = leftSeat_Arm;
+                RightSeat_Arm = rightSeat_Arm;
+                FuelMain_Arm = fuelMain_Arm;
+                FuelAux_Arm = fuelAux_Arm;
+                Baggage_Arm = baggage_Arm;
+            }
+        public void addToTotalMass(double mass) {
+            total_Mass += mass;
+        }
+
+        public void calculateMassArmS() {
+            
+            BEM_MassArm = BEM_Mass * BEM_Arm;
+            total_MassArm += BEM_MassArm;
+
+            LeftSeat_MassArm = LeftSeat_Mass * LeftSeat_Arm;
+            total_MassArm += LeftSeat_MassArm;
+
+            RightSeat_MassArm = RightSeat_Mass * RightSeat_Arm;
+            total_MassArm += RightSeat_MassArm;
+
+            FuelMain_MassArm = FuelMain_Mass * FuelMain_Arm;
+            total_MassArm += FuelMain_MassArm;
+
+            FuelAux_MassArm = FuelAux_Mass * FuelAux_Arm;
+            total_MassArm += FuelAux_MassArm;
+
+            Baggage_MassArm = Baggage_Mass * Baggage_Arm;
+            total_MassArm += Baggage_MassArm;
+        }
+
+        public void calculateTotalArm() {
+            total_Arm = total_MassArm / total_Mass;
+        }
+
     }
 }
--- FlightPlanning_UTSA/Program.cs
+++ FlightPlanning_UTSA/Program.cs
@@ -4,9 +4,88 @@ namespace FlightPlanning_UTSA
 {
     class Program
     {
+        Plane Zlin;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var program = new Program();
+            
+            bool run = true;
+            
+            while(run) {
+                program.runProgram();
+            }
         }
+
+
+        public void runProgram() {
+
+                addPlanes();
+
+                weightAndBalance();
+
+        }
+
+        public void addPlanes(){
+            
+            Zlin = new Plane(0.653, 0.956, 0.956, 0.750, 0.948, 1.766);
+        }
+
+        public void weightAndBalance() {
+            double response;
+
+            response = askUser("Basic empty mass:");
+            Zlin.addToTotalMass(response);
+
+            response = askUser("Left Seat mass:");
+            Zlin.LeftSeat_Mass = response;
+            Zlin.addToTotalMass(response);
+
+            response = askUser("Right Seat mass:");
+            Zlin.RightSeat_Mass = response;
+            Zlin.addToTotalMass(response);
+
+            response = askUser("Main FuelTank mass:");
+            Zlin.FuelMain_Mass = response;
+            Zlin.addToTotalMass(response);
+
+            response = askUser("Auxilary FuelTank mass:");
+            Zlin.FuelAux_Mass = response;
+            Zlin.addToTotalMass(response);
+
+            response = askUser("Baggage mass:");
+            Zlin.Baggage_Mass = response;
+            Zlin.addToTotalMass(response);
+
+            Zlin.calculateMassArmS();
+            Zlin.calculateTotalArm();
+
+            String[] message = {    "Total Mass: " + Zlin.total_Mass.ToString(),
+                                    "Total Arm: " + Zlin.total_Arm,
+                                    "Total MassArm: " + Zlin.total_MassArm};
+
+            Display(message);
+        }
+
+        public double askUser(string question) {
+            double answer;
+
+            Console.WriteLine(question);
+            answer = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(answer);
+
+            return answer;
+        }
+
+        public void Display(String[] message) {
+            foreach (String value in message)
+            {
+                Console.WriteLine(value.ToString());
+            }
+
+        }
+
+
     }
 }
